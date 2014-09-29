e80edd96831f53ffe0368d56e65f5a354bf9c34e
Mike Norman <mnorman@signiant.com>
2014-09-29 17:13:44 -0400

    final code demonstrating Grizzly issue:  serverEntity is not same as clientEntity

--- src/main/java/org/mwnorman/models/Account.java
+++ src/main/java/org/mwnorman/models/Account.java
@@ -1,13 +1,67 @@
 package org.mwnorman.models;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
 public class Account {
 
+    @XmlElement
     protected String id;
+
+    @XmlElement
     protected String description;
+
+    //JAXB needs default constructors
+    public Account() {
+    }
 
     public Account(String id, String description) {
         super();
         this.id = id;
         this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Account)) {
+            return false;
+        }
+        Account other = (Account) obj;
+        if (description == null) {
+            if (other.description != null) {
+                return false;
+            }
+        }
+        else if (!description.equals(other.description)) {
+            return false;
+        }
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        }
+        else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/org/mwnorman/resources/TestResourceTest.java
+++ src/test/java/org/mwnorman/resources/TestResourceTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Logger;
@@ -36,6 +37,7 @@ import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.server.validation.ValidationError;
 import org.glassfish.jersey.servlet.ServletContainer;
 import static org.glassfish.jersey.servlet.ServletProperties.JAXRS_APPLICATION_CLASS;
 
@@ -43,12 +45,14 @@ import static org.glassfish.jersey.servlet.ServletProperties.JAXRS_APPLICATION_C
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 //domain imports
 import org.mwnorman.helpers.TestHelper;
 import org.mwnorman.jersey.JacksonObjectMapperResolver;
+import org.mwnorman.models.Account;
 import static org.mwnorman.jersey.JacksonObjectMapperResolver.getObjectMapper;
 import static org.mwnorman.resources.helpers.ResourceHelper.ID_PATHPARAM;
 import static org.mwnorman.resources.helpers.ResourceHelper.ROOT_RESOURCE;
@@ -58,11 +62,13 @@ import static org.mwnorman.resources.helpers.ResourceHelper.TEST_RESOURCE_PATH;
 public class TestResourceTest  {
     
     static final String TEST_SIMPLE_NAME = TestResourceTest.class.getSimpleName();
+    static Logger logger = Logger.getLogger(TestResourceTest.class.getName());
     
     public static final int PORT = 23456;
     public static final String REST_CONTEXT = "REST";
     public static final String VERSION = ROOT_RESOURCE;
     public static final String BASE_URI = "http://localhost:" + PORT + "/";
+    public static final String INVALID_ID = "this is an invalid id";
     public static final String MOCK_ID = "28cdcb4d-b067-4298-a668-77b9d1060700";
     
     //JUnit fixtures
@@ -72,7 +78,7 @@ public class TestResourceTest  {
     static Client client = null;
     
     @BeforeClass
-    public static void setUpResourceTestBase() throws IOException {
+    public static void setUpGrizzlyServer() throws IOException {
         // build Grizzly server step-by-step instead of using GrizzlyWebContainerFactory -
         // allows us to create the server without auto-starting it, configure things, THEN start it
         WebappContext webappContext = new WebappContext("GrizzlyContext", "");
@@ -107,7 +113,13 @@ public class TestResourceTest  {
                         Scanner scanner = new Scanner(entityStream).useDelimiter("\\A"); //end-of-line
                     )
                     {
-                        clientEntity = scanner.hasNext() ? scanner.next() : "";
+                        String entityString = scanner.hasNext() ? scanner.next() : "";
+                        if (responseContext.getStatus() == 200) {
+                            clientEntity = getObjectMapper().readValue(entityString, Account.class);
+                        }
+                        else {
+                            clientEntity = entityString;
+                        }
                     }
                     catch (IOException e) {
                         // ignore
@@ -135,35 +147,37 @@ public class TestResourceTest  {
      * @GET
      * @Path("root/test/{id}"
      */
-    
-    @Test
-    public void test_getId_invalidId() {
-        WebTarget webResource = client
-            .target(BASE_URI)
-            .path(buildPathToTestSource())
-            .resolveTemplate(ID_PATHPARAM, "this_is_an_invalid_userAccountId")
-        ;
-        Response clientResponse = null;
-        try {
-            clientResponse = webResource
-                .request(MediaType.APPLICATION_JSON)
-                .get();
-            int status = clientResponse.getStatus();
-            assertTrue("GET on {version}/user/{userId}/password did not return BadRequest(" +
-                Response.Status.BAD_REQUEST.getStatusCode() +")",
-                status == Response.Status.BAD_REQUEST.getStatusCode());
-        }
-        catch (Exception e) {
-            fail(e.getMessage());
-        }
-    }
 
     @Test
     public void test_getId_validId() {
         WebTarget webResource = client
             .target(BASE_URI)
             .path(buildPathToTestSource())
-            .resolveTemplate(ID_PATHPARAM, "this_is_an_invalid_userAccountId")
+            .resolveTemplate(ID_PATHPARAM, MOCK_ID)
+        ;
+        Response clientResponse = null;
+        try {
+            clientResponse = webResource
+                .request(MediaType.APPLICATION_JSON)
+                .get();
+            int status = clientResponse.getStatus();
+            assertTrue("GET on {version}/user/{userId}/password did not return Ok(" +
+                Response.Status.OK.getStatusCode() +")",
+                status == Response.Status.OK.getStatusCode());
+            assertEquals(serverEntity, clientEntity);
+        }
+        catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void test_getId_invalidId() {
+        WebTarget webResource = client
+            .target(BASE_URI)
+            .path(buildPathToTestSource())
+            .resolveTemplate(ID_PATHPARAM, INVALID_ID)
         ;
         Response clientResponse = null;
         try {
@@ -174,6 +188,13 @@ public class TestResourceTest  {
             assertTrue("GET on {version}/user/{userId}/password did not return BadRequest(" +
                 Response.Status.BAD_REQUEST.getStatusCode() +")",
                 status == Response.Status.BAD_REQUEST.getStatusCode());
+            List<ValidationError> listOfErrors = (List<ValidationError>)serverEntity;
+            ValidationError validationError = listOfErrors.get(0);
+            assertEquals("'" + INVALID_ID + 
+                "' is not a valid id (xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx where x is any hexadecimal digit and y is one of 8, 9, a (A), or b (B)",
+                validationError.getMessage());
+            //clientEntity should contain validationError's, but doesn't
+            logger.info(clientEntity.toString());
         }
         catch (Exception e) {
             fail(e.getMessage());
@@ -202,7 +223,7 @@ public class TestResourceTest  {
                     serverEntity = responseContext.getEntity();
                 }
             });
-            registerInstances(new LoggingFilter(Logger.getLogger(TestResourceApplication.class.getName()), true));
+            registerInstances(new LoggingFilter(logger, true));
         }
     }
 
